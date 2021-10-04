0e3d0173c106f326f9f4df32229411e25c60ffc0
Ashutosh kumar <krashutosh27@gmail.com>
2021-10-04 16:57:29 +0530

    Setup app for heroku2

--- src/app.js
+++ src/app.js
@@ -95,7 +95,6 @@ app.get('*', (req, res) => {
         errorMessage: 'Page not found.'
     })
 })
-
-app.listen(3000, () => {
-    console.log('Server is up on port 3000.')
+app.listen(port, () => {
+    console.log('Server is up on port ' + port)
 })
